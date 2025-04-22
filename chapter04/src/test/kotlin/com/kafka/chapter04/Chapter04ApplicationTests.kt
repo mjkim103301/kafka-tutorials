65edeafef842a20bca904ae2e1a3b85c3884c83b
김민지 <mjkim103301@gmail.com>
2025-04-22 13:29:55 +0900

    [chore] chapter04 프로젝트 미리 생성

---
+++ chapter04/src/test/kotlin/com/kafka/chapter04/Chapter04ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kafka.chapter04
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter04ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
